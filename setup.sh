b8a2d55cec44158cbdeced1cf195660829a1efbc
Stefanie-A <albertstephanie630@gmail.com>
2026-05-08 15:07:37 +0100

    feat: add rbac to eks cluster

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-aws eks update-kubeconfig --name $(terraform output -raw cluster_name) --region $(terraform output -raw region)
+# aws eks update-kubeconfig --name $(terraform output -raw cluster_name) --region $(terraform output -raw region)
 
-Create the pull secret
-kubectl create secret docker-registry ghcr-pull-secret \
-  --docker-server=ghcr.io \
-  --docker-username=stefanie-a \
-  --docker-password=<your-github-pat> \
-  --docker-email=<your-email> \
-  -n app
+# Create the pull secret
+# kubectl create secret docker-registry ghcr-pull-secret \
+#   --docker-server=ghcr.io \
+#   --docker-username=stefanie-a \
+#   --docker-password=<your-github-pat> \
+#   --docker-email=<your-email> \
+#   -n app
 
-Patch the deployment
-kubectl patch deployment todo-api-deployment -n app \
-  -p '{"spec":{"template":{"spec":{"imagePullSecrets":[{"name":"ghcr-pull-secret"}]}}}}'
+# Patch the deployment
+# kubectl patch deployment todo-api-deployment -n app \
+#   -p '{"spec":{"template":{"spec":{"imagePullSecrets":[{"name":"ghcr-pull-secret"}]}}}}'
--- terraform/module/eks/main.tf
+++ terraform/module/eks/main.tf
@@ -8,14 +8,34 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  enable_irsa = true  
+  enable_irsa = true
 
-  cluster_enabled_log_types = ["api", "audit", "authenticator"]
+  cluster_enabled_log_types = [
+    "api",
+    "audit",
+    "authenticator"
+  ]
 
   cluster_endpoint_public_access = true
 
   enable_cluster_creator_admin_permissions = true
-  
+
+  access_entries = {
+    github_actions = {
+      principal_arn = "arn:aws:iam::902839103466:role/todo-app-prod-cicd-deploy-role"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   eks_managed_node_groups = {
     default = {
       instance_types = [var.node_instance_type]
